4bd83c5d5dd5e8706f0c62e3f4ae2c27a50e6f0c
Tiago Marques <tiago.marques@yieldify.com>
2016-03-17 14:22:42 +0000

    tabs instead of spaces

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
 /*
@@ -27,5 +27,5 @@ Route::get('/', function () {
 */
 
 Route::group(['middleware' => ['web']], function () {
-    //
+	//
 });
