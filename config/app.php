7c2948c37778a20647416f07d2c5be76e7c43961
Tiago Marques <tiago.marques@yieldify.com>
2016-03-21 11:54:51 +0000

    added service providers and aliases

--- config/app.php
+++ config/app.php
@@ -143,6 +143,16 @@ return [
 		App\Providers\AuthServiceProvider::class,
 		App\Providers\EventServiceProvider::class,
 		App\Providers\RouteServiceProvider::class,
+
+		/*
+		 * Collective Service Providers...
+		 */
+		Collective\Html\HtmlServiceProvider::class,
+
+		/*
+		 * Intervention Service Providers...
+		 */
+		Intervention\Image\ImageServiceProvider::class,
 	],
 
 	/*
@@ -189,5 +199,16 @@ return [
 		'URL' => Illuminate\Support\Facades\URL::class,
 		'Validator' => Illuminate\Support\Facades\Validator::class,
 		'View' => Illuminate\Support\Facades\View::class,
+
+		/*
+		 * Collective Aliases...
+		 */
+		'Form' => Collective\Html\FormFacade::class,
+		'Html' => Collective\Html\HtmlFacade::class,
+
+		/*
+		 * Intervention Aliases...
+		 */
+		'Image' => Intervention\Image\Facades\Image::class,
 	],
 ];
