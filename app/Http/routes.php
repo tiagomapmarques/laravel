38ba220cf1d3b49cb6257dfbd89f6a80cf96bc6a
Tiago Marques <tiago.marques@yieldify.com>
2016-03-29 18:12:30 +0100

    updated routes to enable API calls

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,25 @@
  *
  */
 Route::group(['middleware' => ['web']], function () {
-	Route::get('/', function () {
+	Route::get('/', function() {
 		return view('welcome');
 	});
+});
+
+/* --------------------------------------------------------------------------
+ *  API Routes
+ * --------------------------------------------------------------------------
+ *
+ * Here is where you can register all of the routes for your application's
+ * API. Do it the same way as if you were registering a regular route.
+ * With just a pinch of salt in the group statement. Note that the default
+ * action of any non-existing API call is an empty string.
+ *
+ * For debugging purposes only, you can change the middleware to
+ * "api-permissive" in order to skip validations and throttling.
+ *
+ */
+Route::group(['namespace' => 'API', 'prefix' => 'api', 'middleware' => ['api']], function() {
+
+	Route::post('echo', 'DefaultAction@run');
 });
