29ddd187c9038f9ef7b6ddc604ebc7a96984e886
Tiago Marques <tiago.marques@yieldify.com>
2016-04-02 18:25:41 +0100

    updated authentication middleware

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -15,8 +15,8 @@ class RedirectIfAuthenticated {
 	 * @return mixed
 	 */
 	public function handle($request, Closure $next, $guard = null) {
-		if (Auth::guard($guard)->check()) {
-			return redirect('/');
+		if(Auth::guard($guard)->check()) {
+			return redirect()->back();
 		}
 
 		return $next($request);
