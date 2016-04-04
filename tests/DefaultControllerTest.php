426e9fbb36a730b68518fc19f31c18e1f0a4750c
Tiago Marques <tiago.marques@yieldify.com>
2016-04-04 21:26:42 +0100

    default controller test update

--- tests/DefaultControllerTest.php
+++ tests/DefaultControllerTest.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+use Faker\Factory as Faker;
+
 class DefaultControllerTest extends TestCase {
 	/**
 	 * Test the reponse of the home page
@@ -23,26 +25,5 @@ class DefaultControllerTest extends TestCase {
 	public function testContent() {
 		$this->visit('/')
 			->see('Laravel Up and Running Kit');
-	}
-
-	/**
-	 * Test that menu exists through the search form
-	 *
-	 * @return void
-	 */
-	public function testMenu() {
-		$text = 'LURK is comming';
-		$final_uri = $this->baseUrl.'/search?q='.str_replace(' ','+',$text);
-		try {
-			$this->visit('/')
-				->type($text, 'q')
-				->press('Submit');
-		}
-		catch(\Exception $e) {
-			if(strpos($e->getMessage(),'A request to ['.$final_uri.'] failed. Received status code [404]')<0) {
-				throw $e;
-			}
-		}
-		$this->assertEquals($this->currentUri,$final_uri);
 	}
 }
