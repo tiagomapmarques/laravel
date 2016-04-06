c7db9ffb499f97e60290b54ffd5db3f4e92d50b0
Tiago Marques <tiago.marques@yieldify.com>
2016-04-06 18:05:17 +0100

    versioning on both css and js files

--- gulpfile.js
+++ gulpfile.js
@@ -11,10 +11,12 @@ var elixir = require('laravel-elixir');
  *
  */
 elixir(function(mix) {
-	mix.sass('app.scss').version([
-		'public/css/app.css'
-	]);
+	mix.sass('app.scss');
 	mix.scripts([ 'search.js', 'app.js' ],
 		'public/js/app.js'
 	);
+	mix.version([
+		'public/css/app.css',
+		'public/js/app.js'
+	]);
 });
