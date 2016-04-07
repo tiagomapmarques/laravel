4035c573bd5cc8a8866a111b22e0fddb8f2f5496
Tiago Marques <tiago.marques@yieldify.com>
2016-04-08 00:07:13 +0100

    added search button on search index

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -3,8 +3,13 @@
 @section('content')
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-md-8 col-md-offset-2">
-				@include('layouts.__search')
+			<div class="col-md-8 col-md-offset-2" style="display: flex;">
+				<div id="search-bar" style="width: 75%;">
+					@include('layouts.__search')
+				</div>
+				<button id="search-button-bar" style="width: 25%; margin-top: 3px; margin-bottom: 17px; margin-left: 10px;">
+					<span class="fa fa-search"></span>
+				</button>
 			</div>
 		</div>
 		<div class="row">
@@ -16,7 +21,7 @@
 				<div class="col-md-8 col-md-offset-2">
 					<div class="panel panel-default">
 						<div class="panel-heading">
-							<h3 class="panel-title" style="text-align: center;">{{ $class_name }}</h3>
+							<h3 class="panel-title" style="text-align: center;">{{ $class_name }} {{ Helper::trans('common.result',2) }}</h3>
 						</div>
 						<div class="panel-body" style="text-align: center;">
 							@if(count($all)<=0)
