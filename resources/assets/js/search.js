87fcc91b2a7e7875432af1c6dab4f1b6a2598d86
Tiago Marques <tiago.marques@yieldify.com>
2016-04-07 16:39:39 +0100

    updated search functionality on js

--- resources/assets/js/search.js
+++ resources/assets/js/search.js
@@ -5,28 +5,44 @@
 
 // Initializes the search button interaction
 function search_init() {
-	$('#search-button').click(search_button_click);
+	$('#search-button-nav').click(search_button_nav_click);
+	$('#search-button-bar').click(search_button_bar_click);
+	$('#search-cancel-bar').click(search_cancel_bar_click);
 }
 
 // performs the search button click action
-function search_button_click() {
+function search_button_nav_click() {
 	var bar = $('#search-bar');
-	var form = bar.find('form');
-	var input = form.find('input');
-	if(bar.width()>0) {
-		if(input.val().length>0) {
-			form.submit();
-		}
-		else {
-			search_bar_animate('-', form.outerWidth());
-		}
+	if(parseInt(bar.css('marginTop'))<0) {
+		search_bar_show();
 	}
-	else {
-		search_bar_animate('+', form.outerWidth());
+}
+function search_button_bar_click() {
+	var form = $('#search-bar').find('form');
+	form.submit();
+}
+function search_cancel_bar_click() {
+	var bar = $('#search-bar');
+	if(parseInt(bar.css('marginTop'))>=0) {
+		search_bar_hide();
 	}
 }
 
 // animates the search bar
-function search_bar_animate(signal, width) {
-	$('#search-bar').animate({ "width": signal + '=' + width + 'px' });
+function search_bar_show() {
+	var bar = $('#search-bar');
+	search_bar_animate('+', bar.outerHeight());
+}
+function search_bar_hide() {
+	var bar = $('#search-bar');
+	search_bar_animate('-', bar.outerHeight());
+}
+function search_bar_animate(signal, height) {
+	if(signal==='+') {
+		$('#nav .container-fluid').animate({ "opacity": '0' }, 100);
+	}
+	else if(signal==='-') {
+		$('#nav .container-fluid').animate({ "opacity": '1' }, 400);
+	}
+	$('#search-bar').animate({ "margin-top": signal + '=' + height + 'px' }, 300);
 }
