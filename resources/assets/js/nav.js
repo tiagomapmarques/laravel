a640b917f4eb0787df114692059a61ccc83f30b4
Tiago Marques <tiago.marques@yieldify.com>
2016-04-07 23:19:13 +0100

    overhaul of search js

--- resources/assets/js/nav.js
+++ resources/assets/js/nav.js
@@ -1,16 +1,42 @@
 
 /**
- * List of functions to be used for the search mechanism
+ * List of functions to be used for the Lurk Search
  */
 
-// Initializes the search button interaction
-function search_init() {
+// Initializes the search buttons interaction
+function nav_init() {
+	$('.navbar-toggle').click(toggle_nav_click);
 	$('#search-button-nav').click(search_button_nav_click);
 	$('#search-button-bar').click(search_button_bar_click);
 	$('#search-cancel-bar').click(search_cancel_bar_click);
 }
 
-// performs the search button click action
+//
+function toggle_nav_click() {
+	if($('.navbar-toggle').attr('class').indexOf('collapsed')<0) {
+		$('#navbar-toggle-icon-2')
+			.scale(1.25,1)
+			.rotate(-50)
+			.translate(5,0);
+		$('#navbar-toggle-icon-3')
+			.scale(1.25,1)
+			.rotate(50)
+			.translate(-5,6);
+	}
+	else {
+		$('#navbar-toggle-icon-2')
+			.translate(-5,0)
+			.rotate(50)
+			.scale(0.8,1);
+		$('#navbar-toggle-icon-3')
+			.translate(5,-6)
+			.rotate(-50)
+			.scale(0.8,1);
+	}
+
+}
+
+// performs the search buttons' click actions
 function search_button_nav_click() {
 	var bar = $('#search-bar');
 	if(parseInt(bar.css('marginTop'))<0) {
@@ -28,7 +54,7 @@ function search_cancel_bar_click() {
 	}
 }
 
-// animates the search bar
+// functions to animate the search bar
 function search_bar_show() {
 	var bar = $('#search-bar');
 	search_bar_animate('+', bar.outerHeight());
@@ -37,6 +63,8 @@ function search_bar_hide() {
 	var bar = $('#search-bar');
 	search_bar_animate('-', bar.outerHeight());
 }
+
+// search bar animation
 function search_bar_animate(signal, height) {
 	if(signal==='+') {
 		$('#nav .container-fluid').animate({ "opacity": '0' }, 100);
