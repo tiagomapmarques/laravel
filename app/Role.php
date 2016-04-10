c8978626db40fe82ed174c3b9e2deabcc8383d0b
Tiago Marques <tiago.marques@yieldify.com>
2016-04-10 14:05:16 +0100

    added Roles model

---
+++ app/Role.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model {
+	/**
+	 * The attributes that are searchable.
+	 *
+	 * @var array
+	 */
+	public static $searchable = [];
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [];
+
+	/**
+	 * The attributes that should be hidden for arrays.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'name', 'class',
+	];
+}
