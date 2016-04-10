25e6a492b4f47f15d90a85ffe67884013aceb10b
Tiago Marques <tiago.marques@yieldify.com>
2016-04-10 14:54:21 +0100

    added test for admin login/logout

--- tests/AuthControllerTest.php
+++ tests/AuthControllerTest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 use Faker\Factory as Faker;
 
+use App\Role;
+
 class AuthControllerTest extends TestCase {
 
 	use DatabaseTransactions;
@@ -16,12 +18,15 @@ class AuthControllerTest extends TestCase {
 	 * @return void
 	 */
 	public function testLoginLogout() {
+		// test login page 200 response code
 		$this->visit('/login')
 			->assertResponseOk();
 
+		// test logout page 200 response code
 		$this->visit('/logout')
 			->assertResponseOk();
 
+		// test a regular user's login
 		$this->visit('/')
 			->click('#login-button')
 			->seePageIs('/login')
@@ -32,6 +37,36 @@ class AuthControllerTest extends TestCase {
 			->dontSee('Login')
 			->see('Logout');
 
+		// test the logout
+		$this->visit('/')
+			->click('#logout-button')
+			->seePageIs('/')
+			->dontSee('Logout')
+			->see('Login');
+	}
+
+	/**
+	 * Test the admin login and logout screens.
+	 *
+	 * @return void
+	 */
+	public function testAdminLoginLogout() {
+		// test admin login
+		$this->visit('/')
+			->click('#login-button')
+			->seePageIs('/login')
+			->type('admin@example.com', 'email')
+			->type('admin4admin', 'password')
+			->press('Login')
+			->seePageIs('/')
+			->dontSee('Login')
+			->see('Logout');
+
+		// test that the admin can access an admin only area
+		$this->visit('/admin')
+			->see('Lurk Admin');
+
+		// test admin logout
 		$this->visit('/')
 			->click('#logout-button')
 			->seePageIs('/')
@@ -64,15 +99,17 @@ class AuthControllerTest extends TestCase {
 	 * @return void
 	 */
 	public function testRegister() {
+		// create random name, email and password
 		$faker = Faker::create();
-
 		$name = $faker->name;
 		$email = $faker->safeemail;
 		$password = Helper::generateHash();
 
+		// test register page 200 response code
 		$this->visit('/register')
 			->assertResponseOk();
 
+		// test the registration process
 		$this->visit('/')
 			->click('#register-button')
 			->seePageIs('/register')
@@ -85,9 +122,13 @@ class AuthControllerTest extends TestCase {
 			->dontSee('Login')
 			->see('Logout');
 
+		// test that the user was created in the database
+		// and that it is not an admin
+		$Role = Role::where('name', 'user')->first();
 		$this->seeInDatabase('users', [
 			'name' => $name,
-			'email' => $email
+			'email' => $email,
+			'role_id' => $Role->id
 		]);
 	}
 }
