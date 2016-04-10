22f47218293079a6020f86d8ad1b2ae70255da62
Tiago Marques <tiago.marques@yieldify.com>
2016-04-10 14:09:43 +0100

    updated seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,8 +4,8 @@ use Illuminate\Database\Seeder;
 
 use Faker\Factory as Faker;
 
+use App\Role;
 use App\User;
-use App\Administrator;
 
 class DatabaseSeeder extends Seeder {
 	/**
@@ -17,22 +17,33 @@ class DatabaseSeeder extends Seeder {
 		$faker = Faker::create();
 
 		// truncate all the tables
+		DB::table('roles')->truncate();
 		DB::table('users')->truncate();
 		DB::table('password_resets')->truncate();
 
-		// create users
-		$user1 = new User();
-		$user1->name = $faker->name;
-		$user1->email = 'user@example.com';
-		$user1->password = bcrypt('user4user');
-		$user1->save();
+		// create roles
+		$Admin_role = new Role();
+		$Admin_role->name = Config::get('auth.admin_role_prefix');
+		$Admin_role->class = 'Administrator';
+		$Admin_role->save();
+		$User_role = new Role();
+		$User_role->name = 'user';
+		$User_role->class = 'User';
+		$User_role->save();
 
 		// create admins
-		$admin1 = new Administrator();
-		$admin1->name = $faker->name;
-		$admin1->username = 'admin';
-		$admin1->email = 'admin@example.com';
-		$admin1->password = bcrypt('admin4admin');
-		$admin1->save();
+		$Admin1 = new User();
+		$Admin1->name = $faker->name;
+		$Admin1->email = 'admin@example.com';
+		$Admin1->password = bcrypt('admin4admin');
+		$Admin1->role()->associate($Admin_role);
+		$Admin1->save();
+
+		// create users
+		$User1 = new User();
+		$User1->name = $faker->name;
+		$User1->email = 'user@example.com';
+		$User1->password = bcrypt('user4user');
+		$User1->save();
 	}
 }
