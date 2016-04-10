b76155014bbc2d92901ef4819cb9a3d3d71eeba6
Tiago Marques <tiago.marques@yieldify.com>
2016-04-10 14:54:58 +0100

    added test environment

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
+
 class TestCase extends Illuminate\Foundation\Testing\TestCase {
 	/**
 	 * The base URL to use while testing the application.
@@ -14,10 +16,23 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 	 * @return \Illuminate\Foundation\Application
 	 */
 	public function createApplication() {
+		putenv('DB_DEFAULT=testing');
+
 		$app = require __DIR__.'/../bootstrap/app.php';
 
 		$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
 		return $app;
+	}
+
+	public function setUp() {
+		parent::setUp();
+		Artisan::call('migrate');
+		Artisan::call('db:seed');
+	}
+
+	public function tearDown() {
+		Artisan::call('migrate:reset');
+		parent::tearDown();
 	}
 }
