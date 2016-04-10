ebf961b82e73df8750f3570d12ac2a4717f1b464
Tiago Marques <tiago.marques@yieldify.com>
2016-04-10 10:56:58 +0100

    added seeder for admins

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
 
 use App\User;
+use App\Administrator;
 
 class DatabaseSeeder extends Seeder {
 	/**
@@ -25,5 +26,13 @@ class DatabaseSeeder extends Seeder {
 		$user1->email = 'user@example.com';
 		$user1->password = bcrypt('user4user');
 		$user1->save();
+
+		// create admins
+		$admin1 = new Administrator();
+		$admin1->name = $faker->name;
+		$admin1->username = 'admin';
+		$admin1->email = 'admin@example.com';
+		$admin1->password = bcrypt('admin4admin');
+		$admin1->save();
 	}
 }
